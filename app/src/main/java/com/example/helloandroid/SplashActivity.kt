f60c60577d05d37c13f014bb69f86ccceba6f97c
germansotelo21 <german@cotech.com.co>
2021-09-02 13:37:44 -0500

    estructura layouts

---
+++ app/src/main/java/com/example/helloandroid/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+    }
+}
